b1fafe7db629d6a0cd176de87b907f3be6fc3993
Darko Božidar <darko.bozidar@gmail.com>
2025-05-14 21:09:36 +0200

    Improved AssetMeasurement endpoints.

--- asset/controllers/measurementControllers.go
+++ asset/controllers/measurementControllers.go
@@ -7,7 +7,6 @@ import (
 
     "encoding/json"
     "net/http"
-    "strconv"
     "context"
     "time"
     "log"
@@ -20,9 +19,9 @@ import (
 
 // TODO check to create indexes.
 func GetLatestMeasurement(c *gin.Context) {
-    assetID, err := strconv.Atoi(c.Param("id"))
+    assetID, err := utils.StringToUint(c.Param("id"))
     if err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": "invalid asset_id"})
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
         return
     }
 
@@ -46,62 +45,11 @@ func GetLatestMeasurement(c *gin.Context) {
 }
 
 func GetMeasurementsInRange(c *gin.Context) {
-    assetID, err := strconv.ParseInt(c.Param("id"), 10, 64)
-    if err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": "invalid asset_id"})
-        return
-    }
-
-    fromStr := c.Query("from")
-    toStr := c.Query("to")
-    order := c.DefaultQuery("order", "asc")
-
-    from, err1 := time.Parse(time.RFC3339, fromStr)
-    to, err2 := time.Parse(time.RFC3339, toStr)
-    if err1 != nil || err2 != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": "invalid from/to date format (expected RFC3339)"})
-        return
-    }
-
-    sortOrder := 1
-    if order == "desc" {
-        sortOrder = -1
-    }
-
-    collection := config.MongoC.Database("asset_measurements").Collection("measurements")
-    filter := bson.M{
-        "asset_id": assetID,
-        "timestamp": bson.M{
-            "$gte": from,
-            "$lte": to,
-        },
-    }
-
-    cursor, err := collection.Find(
-        context.TODO(),
-        filter,
-        options.Find().SetSort(bson.D{{"timestamp", sortOrder}}),
-    )
-    if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "query failed"})
-        return
-    }
-
-    var results []models.AssetMeasurement
-    if err := cursor.All(context.TODO(), &results); err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "decoding failed"})
-        return
-    }
-
-    c.JSON(http.StatusOK, results)
-}
-
-func GetAverageMeasurements(c *gin.Context) {
     var queryParams struct {
         From     string `form:"from" binding:"required,datetime=2006-01-02T15:04:05Z07:00"`
         To       string `form:"to" binding:"required,datetime=2006-01-02T15:04:05Z07:00"`
-        GroupBy  string    `form:"groupBy" binding:"omitempty,oneof=1minute 15minute 1hour"`
-        Sort     string    `form:"sort" binding:"omitempty,oneof=asc desc"`
+        GroupBy  string `form:"groupBy" binding:"omitempty,oneof=1minute 15minute 1hour"`
+        Sort     string `form:"sort" binding:"omitempty,oneof=asc desc"`
     }
 
     if err := c.ShouldBindQuery(&queryParams); err != nil {
@@ -124,6 +72,72 @@ func GetAverageMeasurements(c *gin.Context) {
         c.JSON(http.StatusBadRequest, gin.H{"error": "invalid 'to' date format (expected RFC3339)"})
     }
 
+    sortOrder := 1
+    if queryParams.Sort == "desc" {
+        sortOrder = -1
+    }
+
+    collection := config.MongoC.Database("asset_measurements").Collection("measurements")
+    filter := bson.M{
+        "asset_id": assetID,
+        "timestamp": bson.M{
+            "$gte": fromDateTime,
+            "$lte": toDateTime,
+        },
+    }
+
+    cursor, err := collection.Find(
+        context.TODO(),
+        filter,
+        options.Find().SetSort(bson.D{{"timestamp", sortOrder}}),
+    )
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Query failed"})
+        return
+    }
+
+    var results []models.AssetMeasurement
+    if err := cursor.All(context.TODO(), &results); err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Decoding failed"})
+        return
+    }
+
+    c.JSON(http.StatusOK, results)
+}
+
+func GetAverageMeasurements(c *gin.Context) {
+    var queryParams struct {
+        From     string `form:"from" binding:"required,datetime=2006-01-02T15:04:05Z07:00"`
+        To       string `form:"to" binding:"required,datetime=2006-01-02T15:04:05Z07:00"`
+        GroupBy  string `form:"groupBy" binding:"omitempty,oneof=1minute 15minute 1hour"`
+        Sort     string `form:"sort" binding:"omitempty,oneof=asc desc"`
+    }
+
+    if err := c.ShouldBindQuery(&queryParams); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    assetID, err := utils.StringToUint(c.Param("id"))
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    fromDateTime, err := time.Parse(time.RFC3339, queryParams.From)
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "invalid 'from' date format (expected RFC3339)"})
+    }
+    toDateTime, err := time.Parse(time.RFC3339, queryParams.To)
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "invalid 'to' date format (expected RFC3339)"})
+    }
+
+    sortOrder := 1
+    if queryParams.Sort == "desc" {
+        sortOrder = -1
+    }
+
     binSize, unit := 1, "minute"
     if queryParams.GroupBy != "" {
         binSize, unit, err = utils.ExtractBinSizeAndUnit(queryParams.GroupBy)
@@ -131,11 +145,6 @@ func GetAverageMeasurements(c *gin.Context) {
             c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
             return
         }
-    }
-
-    sortOrder := 1
-    if queryParams.Sort == "desc" {
-        sortOrder = -1
     }
 
     // TODO fix this.
@@ -173,7 +182,7 @@ func GetAverageMeasurements(c *gin.Context) {
 
     cursor, err := collection.Aggregate(context.TODO(), pipeline)
     if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "Aggregation failed"})
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Query failed"})
         return
     }
 
